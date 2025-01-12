ff4f3377a4706ffee1c5449eb43ff33cc8c76f44
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-01-12 14:00:54 +0300

    добавлены тесты инициализации для класса product

---
+++ tests/test_product.py
@@ -0,0 +1,9 @@
+import pytest
+
+from src.product import Product
+
+def test_product_init(product):
+    assert product.name == "something"
+    assert product.description == "useful tool for testing"
+    assert product.price == 125.50
+    assert product.quantity == 666
