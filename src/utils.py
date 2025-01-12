dcb8cb40274faefc359079384d4aec2f2ad93340
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-01-12 14:52:41 +0300

    добавлены функции json_reader и object_creator

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+from src.product import Product
+from src.category import Category
+
+
+def json_reader(path: str):
+    full_path = os.path.abspath(path)
+    with open(full_path, "r", encoding="UTF-8") as file:
+        data = json.load(file)
+    return data
+
+
+def object_creator(data):
+    categories = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(**product))
+        category["products"] = products
+        categories.append(Category(**category))
+    return categories
