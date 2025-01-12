d78e6d36a68874d118808c6ebf357a2670979481
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-01-12 13:20:23 +0300

    добавлены атрибуты count_category и count_of_goods для класса Category

--- category.py
+++ category.py
@@ -3,9 +3,13 @@ class Category:
     name = str
     description = str
     products = list
+    count_category = 0
+    count_of_goods = 0
 
     def __init__(self, name, description, products):
         """Метод для инициализации экземпляра класса Category. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.products = products
+        count_category += 1
+        count_of_goods += len(products)
