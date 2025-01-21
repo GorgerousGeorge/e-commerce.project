b5b3e05f6e3e33a524c135cc07c55d26ee56b725
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-01-21 20:47:58 +0300

    Добавлены методы add_product и products_in_string; атрибут self.product сделан приватным

--- src/category.py
+++ src/category.py
@@ -10,6 +10,19 @@ class Category:
         """Метод для инициализации экземпляра класса Category. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.count_category += 1
         Category.count_of_goods += len(products)
+
+    def add_product(self, new_product):
+        """Метод для записи новых объектов класса Product в атрибут Category.products"""
+        self.products.append(new_product)
+
+
+    @property
+    def products_in_string(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
