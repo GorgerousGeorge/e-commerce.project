58f8c3ee01b0e79dd22838c17e29c54bb3986e9c
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-02-01 15:27:28 +0300

    добавлены тесты для сеттера и геттера price

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,10 +1,12 @@
 from src.product import Product
 
+
 def test_product_init(product):
     assert product.name == "something"
     assert product.description == "useful tool for testing"
     assert product.price == 125.50
     assert product.quantity == 666
+
 
 def test_new_product(product, product_in_dict, first_list_products, second_list_products, third_list_products):
     assert product.new_product(product_in_dict, third_list_products).quantity == 14
@@ -17,3 +19,10 @@ def test_new_product(product, product_in_dict, first_list_products, second_list_
     assert product.new_product(product_in_dict, second_list_products).price == 9999999.99
 
 
+def test_price(capsys, product):
+    assert product.price == 125.50
+    product.price = 255
+    assert product.price == 255
+    product.price = -155
+    message = capsys.readouterr()
+    assert message.out == "Цена не должна быть нулевая или отрицательная\n"
