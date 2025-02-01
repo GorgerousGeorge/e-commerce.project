cdb85e8e2d40c8730cf13e5c6201b738e314e3d4
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-02-01 15:28:21 +0300

    чистка кода от артефактов

--- src/category.py
+++ src/category.py
@@ -32,16 +32,3 @@ class Category:
     @property
     def products_in_list(self):
         return self.__products
-
-if __name__ == "__main__":
-    product1 = Product("something", "useful tool for testing", 125.50, 666)
-    product2 = Product("anything", "everything you desire", 9999999.99, 1)
-    product3 = Product("everything", "everything everywhere and at once", 69.77, 13)
-    product4 = Product("nothing", "respectfully accepting donations", 100, 34435353)
-
-    Category1 = Category("test", "testing category", [product1, product2, product3])
-    print(Category1.products_in_string)
-    print(Category1.count_of_goods)
-    Category1.add_product(product4)
-    print(Category1.products_in_string)
-    print(Category1.count_of_goods)
