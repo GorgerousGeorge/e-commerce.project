f51d17dc39a97368ed97ecc4730ec452d29ebc47
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-02-02 12:02:59 +0300

    добавлен метод str для класса product и доработан геттер для класса category

--- src/product.py
+++ src/product.py
@@ -12,6 +12,11 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+
+    def __str__(self):
+        return f"{self.name}, {self.price}. Остаток: {self.quantity} шт."
+
+
     @classmethod
     def new_product(cls, dict_of_product, list_of_products: list = []):
         """Метод для создания новых объектов класса Product. На вход необходимо подать словарь с параметрами товаров:
