f945e06260eded6d159dbcfe87998919e908d092
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-02-16 19:49:57 +0300

    добавлено покрытие тестами для новог офункционала

--- src/baseproduct.py
+++ src/baseproduct.py
@@ -1,4 +1,4 @@
-from ABC import abc, abstractmethod
+from abc import ABC, abstractmethod
 
 
 class BaseProduct(ABC):
--- src/mixin_for_print.py
+++ src/mixin_for_print.py
@@ -3,4 +3,4 @@ class Mixinforprint:
         print(repr(self))
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.__price}, {self.quantity})"
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
